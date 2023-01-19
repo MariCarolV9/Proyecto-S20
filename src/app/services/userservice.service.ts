009ea4a96973c652f313e3fd1cd97eaa6499e4bd
MariCarolV9 <118744765+MariCarolV9@users.noreply.github.com>
2023-01-19 14:14:36 -0500

    Add files via upload

---
+++ src/app/services/userservice.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Users } from '../models/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserserviceService {
+
+ // Crear una instancia de HttpClient
+constructor(private http: HttpClient) { }
+
+// Definir la URL (EndPoint) de la API (La BaseURL de la API estara en environment.ts)
+apiUser = environment.apiURL + 'posts/';
+
+
+  // Metodo GET para el Interceptor
+  //Solicitando un perdido para 
+  getUsersAllInterceptor():Observable<any>{
+    return this.http.get(this.apiUser, {observe: 'response'});
+    }
+  }
+
+
+
